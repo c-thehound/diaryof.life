1b0f18b7df73cdacb2e1d9e554d6a548af28e0fe
boatcoder@gmail.com <Mark Jones>
2019-02-20 20:19:44 -0500

    Minor tweaks, added a key= for each story, added ability to clip the html without breaking all the tags

--- src/scenes/stories/stories.js
+++ src/scenes/stories/stories.js
@@ -1,125 +1,135 @@
-import React from 'react';
-import Navigation from '../../components/navigation/navigation';
-import { Grid,Card, Image, Menu, Button} from 'semantic-ui-react';
-import './stories.css';
-import {connect} from 'react-redux';
-import {fetchStories} from '../../services/store/actions/storyactions';
-import formatDateTime from '../../utils/formatDateTime';
-import {NavLink} from 'react-router-dom';
+import React from "react";
+import Navigation from "../../components/navigation/navigation";
+import { Grid, Card, Image, Menu, Button } from "semantic-ui-react";
+import "./stories.css";
+import { connect } from "react-redux";
+import { fetchStories } from "../../services/store/actions/storyactions";
+import formatDateTime from "../../utils/formatDateTime";
+import { NavLink } from "react-router-dom";
+import { authorCache } from "../../services/AuthorCache";
+import clip from "text-clipper";
 
-class Author extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = {
-            author:{}
-        }
-    }
-
-    componentDidMount(){
-        this.fetchAuthor(this.props.url)
-    }
-
-    fetchAuthor =(url) =>{
-        fetch(url)
-        .then(res => res.json())
-        .then((response) =>{
-            console.log(response);
-            this.setState({
-                author:response
-            })
-        })
-    }
-
-    render(){
-        const {author} = this.state;
-        return(
-            <div>
-            <Image avatar src={author.avatar}/>
-            <span className="detailone">
-            <p className="author">
-            {author.name}
-            </p>
-            </span>
-            </div>
-        );
-    }
+function StoryLink(props) {
+  let { story } = props;
+  return (
+    <NavLink
+      to={{
+        pathname: `/story/${story.title
+          .split(" ")
+          .join("-")
+          .toLowerCase()}/`,
+        state: { story_object: story }
+      }}
+    >
+      <p className="story_title">{story.title}</p>
+    </NavLink>
+  );
 }
 
+class Author extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      author: {}
+    };
+  }
 
-class Stories extends React.Component{
+  componentDidMount() {
+    this.fetchAuthor(this.props.url);
+  }
 
-    componentDidMount(){
-        this.props.fetchStories();
-    }
-    
-    formatDate = (date) =>{
-        var date_string = formatDateTime(date);
-        return date_string;
-    }
+  fetchAuthor = url => {
+    authorCache.getAuthor(url).then(response => {
+      this.setState({
+        author: response
+      });
+    });
+  };
 
-    reduceStoryText = (text) =>{
-        var maxlen = 200;
-        var substring = text.substring(0,maxlen);
-        return substring+"...";
-    }
-  
+  render() {
+    const { author } = this.state;
+    return (
+      <div>
+        <Image avatar src={author.avatar} />
+        <span className="detailone">
+          <p className="author">{author.name}</p>
+        </span>
+      </div>
+    );
+  }
+}
 
-    render(){
-        const {stories={}} = this.props;
-        return(
-            <Navigation>
-            <div className="stories app-content">
-            <Grid stackable>
+class Stories extends React.Component {
+  componentDidMount() {
+    this.props.fetchStories();
+  }
+
+  formatDate = date => {
+    var date_string = formatDateTime(date);
+    return date_string;
+  };
+
+  reduceStoryText = htmlString => {
+    return clip(htmlString, 150, { html: true });
+  };
+
+  render() {
+    const { stories = {} } = this.props;
+    return (
+      <Navigation>
+        <div className="stories app-content">
+          <Grid stackable>
             <Grid.Row className="storiesmenu">
-                <Menu>
-                    <Menu.Item>
-                        <Button as="a">
-                        Featured
-                        </Button>
-                    </Menu.Item>
-                </Menu>
+              <Menu>
+                <Menu.Item>
+                  <Button as="a">Featured</Button>
+                </Menu.Item>
+              </Menu>
             </Grid.Row>
-            <Grid.Row >
-                <Grid.Column width={10}>
-                {stories.results ? 
-                    <div>
-                        {stories.results.map(story => (
-                        <Card>
-                            <Card.Content>
-                                <NavLink to={{pathname:`/story/${story.title.split(" ").join('-').toLowerCase()}/`,state:{story_object:story}}}>
-                                    <p className="story_title">{story.title}</p>
-                                </NavLink>
-                                    <p className="tagline">{story.tagline}</p>
-                                    <div className="snippet">
-                                    <p>
-                                        {this.reduceStoryText(story.text)}
-                                    </p>
-                                    </div>
-                            </Card.Content>
-                            <Card.Content extra>
-                                <Author url={story.author}/>
-                                <p className="published">{this.formatDate(story.published_at)}</p>
-                            </Card.Content>
-                        </Card>
-                ))}
-                    </div>
-                    :null}
-                </Grid.Column>
-                <Grid.Column width={6}>
-                
-                </Grid.Column>
+            <Grid.Row>
+              <Grid.Column width={10}>
+                {stories.results ? (
+                  <div>
+                    {stories.results.map(story => (
+                      <Card key={story.url}>
+                        <Card.Content>
+                          <StoryLink story={story} />
+                          <p className="tagline">{story.tagline}</p>
+                          <div className="snippet">
+                            <p
+                              dangerouslySetInnerHTML={{
+                                __html: this.reduceStoryText(story.html)
+                              }}
+                            />
+                          </div>
+                        </Card.Content>
+                        <Card.Content extra>
+                          <Author url={story.author} />
+                          <p className="published">
+                            {this.formatDate(story.published_at)}
+                          </p>
+                        </Card.Content>
+                      </Card>
+                    ))}
+                  </div>
+                ) : null}
+              </Grid.Column>
+              <Grid.Column width={6} />
             </Grid.Row>
-            </Grid>
-            </div>
-            </Navigation>
-        );
-    }
+          </Grid>
+        </div>
+      </Navigation>
+    );
+  }
 }
 
-const mapStateToProps = state =>{
-    return{
-        stories:state.stories.stories
-    }
-}
+const mapStateToProps = state => {
+  return {
+    stories: state.stories.stories
+  };
+};
 
-export default connect(mapStateToProps,{fetchStories})(Stories);
+export default connect(
+  mapStateToProps,
+  { fetchStories }
+)(Stories);
